7eb6d08c12068cd30b49c1821d868f61638a14e7
MayankShah1 <mayankshah11996@gmail.com>
2017-01-03 17:49:07 +0000

    Scaffolded the to-do list and routed it

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root 'to_do_lists#index'
+  resources :to_do_lists
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
