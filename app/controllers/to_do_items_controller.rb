6ef8d6a6640f91949b6d40ae6ef9a185a5fb535a
MayankShah1 <mayankshah11996@gmail.com>
2017-01-05 17:44:35 +0000

    Added the functionality to create to do item in to do list

--- app/controllers/to_do_items_controller.rb
+++ app/controllers/to_do_items_controller.rb
@@ -1,2 +1,20 @@
 class ToDoItemsController < ApplicationController
+    before_action :set_to_do_list
+
+    def create
+        @to_do_item = @to_do_list.to_do_items.create(to_do_item_params)
+        
+        # Redirect back to the to_do_list
+        redirect_to @to_do_list
+    end
+    
+    private
+        
+    def set_to_do_list
+        @to_do_list = ToDoList.find(params[:to_do_list_id])
+    end
+
+    def to_do_item_params
+        params[:to_do_item].permit(:content)
+    end
 end
